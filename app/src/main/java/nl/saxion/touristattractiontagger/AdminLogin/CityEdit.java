0a13c4ee71cb826dbdeb83a0e2732678eba3c25c
PetarMihaylovski <459725@student.saxion.nl>
2019-04-07 19:01:49 +0200

    Switching screen from CityEdit to AddCity screen

--- app/src/main/java/nl/saxion/touristattractiontagger/AdminLogin/CityEdit.java
+++ app/src/main/java/nl/saxion/touristattractiontagger/AdminLogin/CityEdit.java
@@ -1,26 +1,41 @@
 package nl.saxion.touristattractiontagger.AdminLogin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import nl.saxion.touristattractiontagger.Adapters.CityDisplayAdapter;
 import nl.saxion.touristattractiontagger.DataProvider.DataProvider;
 import nl.saxion.touristattractiontagger.R;
 
 public class CityEdit extends AppCompatActivity {
+    private Button btnAddCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_edit);
 
-        TextView tvNotification = findViewById(R.id.tvNotification);
+        btnAddCity = findViewById(R.id.btnAddCity);
+        //creating the list view and adding an adapter.
         ListView lvCities = findViewById(R.id.lvCities);
-        Button addCity = findViewById(R.id.btnAddCity);
         CityDisplayAdapter adapter = new CityDisplayAdapter(this, DataProvider.CITIES);
         lvCities.setAdapter(adapter);
+        addCityOnClickListener();
+    }
+
+    private void addCityOnClickListener(){
+        this.btnAddCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent switchScreen = new Intent(CityEdit.this, AddCity.class);
+                startActivity(switchScreen);
+            }
+        });
     }
 }
+
+
